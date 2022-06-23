e9ce489005e4c5e239da57d0b9da4104b12dfcb2
Gabriel Sousa <79923219+Gabriel-Sousa@users.noreply.github.com>
2022-06-23 14:48:51 -0300

    Update Video.tsx

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -4,6 +4,7 @@ import {
   CaretRight,
   DiscordLogo,
   FileArrowDown,
+  Image,
   Lightning,
 } from "phosphor-react";
 import "@vime/core/themes/default.css";
@@ -134,7 +135,7 @@ export function Video({ lessonSlug }: VideoProps) {
             className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors"
           >
             <div className="bg-green-700 h-full p-6 flex items-center">
-              <FileArrowDown size={40} />
+              <Image size={40} />
             </div>
             <div className="py-6 leading-relaxed">
               <strong className="text-2xl">Material complementar</strong>
